6d9913ec4b9ab7f992ebc94b98373274a3c417b9
munim129 <munim129@users.noreply.github.com>
2020-06-03 02:23:20 +0500

    Latest.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br />
-          Updated by Munim
+          Updated by Munim.
         </p>
         <a
           className="App-link"
